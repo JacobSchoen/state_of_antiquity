b8a3d6acee3354513c759f9e67b06a70c655e660
JacobSchoen <schoen.jacob@gmail.com>
2023-06-25 23:42:30 -0400

    update Navbar per client request

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -21,21 +21,19 @@ export default async function RootLayout({
 
   return (
     <html lang="en">
-      <body className="max-w-3xl mx-auto py-10 bg-bisque		">
-        <header className="px-4 py-2 flex items-center justify-between bg-rosybrown border-2 border-gray-500 rounded-full">
-          <div className="flex items-center gap-5 text-sm text-gray-600">
-            {pages.map((page) => (
-              <Link
-                href={page.route}
-                className="text-lg font-bold hover:underline"
-              >
-                {page.name}
-              </Link>
-            ))}
-          </div>
+      <body className="max-w-3xl mx-auto py-10 bg-bisque">
+        <header className="px-4 py-2 gap-5 justify-center flex text-center text-gray-600">
+          {pages.map((page) => (
+            <Link
+              href={page.route}
+              className="text-lg font-bold hover:underline"
+            >
+              {page.name}
+            </Link>
+          ))}
         </header>
 
-        <main className="py-20">{children}</main>
+        <main className="">{children}</main>
       </body>
     </html>
   );
